e8317816eefcd23f02253d25b6e258da18e71ae7
Leotrimbehrami <116160212+Leotrimbehrami@users.noreply.github.com>
2024-02-18 23:59:40 +0100

    added the hooks components

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnInit, OnChanges, AfterContentInit, AfterViewInit {
 
   constructor() { }
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges called');
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit called');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called');
   }
 
 }
