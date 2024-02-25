5163bfc99f50c4209f05adb303a0a52c7073ecba
Leotrimbehrami <116160212+Leotrimbehrami@users.noreply.github.com>
2024-02-25 17:27:58 +0100

    designed the movie details page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { LoaderComponent } from './components/loader/loader.component';
 import { ModalComponent } from './components/modal/modal.component';
 import { ProfitPipe } from './pipes/profit.pipe';
 import { FormsModule } from '@angular/forms';
+import { DetailsActorsComponent } from './components/details-actors/details-actors.component';
 
 @NgModule({
   declarations: [
@@ -33,6 +34,7 @@ import { FormsModule } from '@angular/forms';
 
     // Pipes
     ProfitPipe,
+      DetailsActorsComponent,
   ],
   imports: [
     BrowserModule,
